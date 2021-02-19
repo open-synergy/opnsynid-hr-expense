2ed7c98157639eae2babbdd35a916322514769ee
Andhitia Rama <andhitia.r@gmail.com>
2021-02-20 03:25:50 +0700

    [hr_reimbursement] pre-commit fix

--- hr_reimbursement/__openerp__.py
+++ hr_reimbursement/__openerp__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2020 OpenSynergy Indonesia
 # Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
@@ -6,8 +5,8 @@
 {
     "name": "Employee Reimbursement",
     "version": "8.0.1.6.0",
-    "website": "https://opensynergy-indonesia.com",
-    "author": "OpenSynergy Indonesia",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
     "license": "AGPL-3",
     "installable": True,
     "application": True,
