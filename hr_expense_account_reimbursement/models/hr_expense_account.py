3ea38d7cbee881056722b5ba938982dc0e659edf
Andhitia Rama <andhitia.r@gmail.com>
2021-11-29 03:12:58 +0700

    [hr_expense_account_reimbursement] 8.0.1.0.0

---
+++ hr_expense_account_reimbursement/models/hr_expense_account.py
@@ -0,0 +1,53 @@
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, fields, models
+
+
+class HrExpenseAccount(models.Model):
+    _name = "hr.expense_account"
+    _inherit = "hr.expense_account"
+
+    @api.depends(
+        "reimbursement_line_ids",
+        "reimbursement_line_ids.price_subtotal",
+        "reimbursement_line_ids.reimbursement_id.state",
+    )
+    def _compute_amount_reimbursement(self):
+        for record in self:
+            result = 0.0
+            for line in record.reimbursement_line_ids:
+                result += line.price_subtotal
+            record.amount_reimbursement = result
+
+    @api.depends(
+        "amount_limit",
+        "amount_reimbursement",
+    )
+    def _compute_amount(self):
+        for record in self:
+            amount_residual = 0.0
+            amount_realize = record.amount_reimbursement
+            record.amount_realize = amount_realize
+            amount_residual = record.amount_limit - amount_realize
+            record.amount_residual = amount_residual
+
+    reimbursement_line_ids = fields.One2many(
+        string="Reimbursement Lines",
+        comodel_name="hr.reimbursement_line",
+        inverse_name="expense_account_id",
+        domain=[("reimbursement_id.state", "not in", ["cancel", "terminate"])],
+    )
+    amount_reimbursement = fields.Float(
+        string="Amount Reimbursement",
+        compute="_compute_amount_reimbursement",
+        store=True,
+    )
+    amount_realize = fields.Float(
+        compute="_compute_amount",
+        store=True,
+    )
+    amount_residual = fields.Float(
+        compute="_compute_amount",
+        store=True,
+    )
