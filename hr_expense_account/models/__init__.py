81fde4b3337e17975d1c3ddd2a35cab541876c75
Andhitia Rama <andhitia.r@gmail.com>
2021-11-29 03:10:52 +0700

    [hr_expense_account] 8.0.1.0.0

---
+++ hr_expense_account/models/__init__.py
@@ -0,0 +1,8 @@
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import (
+    hr_expense_account_type,
+    hr_expense_account,
+)
