d0fc7a18dcfb9564b2057c375d40d5071ff27fc5
mikevhe18 <viriyananda.michael@gmail.com>
2022-12-16 12:08:18 +0700

    [ssi_hr_expense] 14.0.1.2.0

--- ssi_hr_expense/models/hr_expense_type.py
+++ ssi_hr_expense/models/hr_expense_type.py
@@ -72,3 +72,28 @@ class HrExpenseType(models.Model):
         string="Default Product Usage",
         comodel_name="product.usage_type",
     )
+
+    analytic_account_method = fields.Selection(
+        string="Analytic Account Selection Method",
+        selection=[
+            ("fixed", "Fixed"),
+            ("python", "Python Code"),
+        ],
+        default="fixed",
+    )
+    analytic_account_ids = fields.Many2many(
+        string="Analytic Accounts",
+        comodel_name="account.analytic.account",
+        relation="rel_expense_type_2_analytic_account",
+        column1="expense_type_id",
+        column2="analytic_account_id",
+    )
+    python_code = fields.Text(
+        string="Python Code",
+        default="""# Available variables:
+#  - env: Odoo Environment on which the action is triggered.
+#  - document: record on which the action is triggered; may be void.
+#  - result: Return result, the value is list of Analytic Accounts.
+result = []""",
+        copy=True,
+    )
