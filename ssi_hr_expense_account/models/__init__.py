d3a1b9ab68f9546aa13ad0a88e1bbc88361d482d
usa8bit <usa8bit@gmail.com>
2023-02-15 15:41:52 +0700

    [ssi_hr_expense_account] 14.0.1.0.0

---
+++ ssi_hr_expense_account/models/__init__.py
@@ -0,0 +1,11 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from . import (
+    employee_expense_account,
+    employee_expense_account_type,
+    employee_expense_account_type_field,
+    hr_expense_type_product,
+    hr_expense_type_product_category,
+)
