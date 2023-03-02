fda6a1cc96a93c23dceefac9c3d71944f374d50b
usa8bit <usa8bit@gmail.com>
2023-03-02 17:22:16 +0700

    [ssi_hr_expense_account_reimbursement] 14.0.1.0.0

--- ssi_hr_expense_account_cash_advance/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright 2023 OpenSynergy Indonesia
-# Copyright 2023 PT. Simetri Sinergi Indonesia
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-
-from . import employee_expense_account, hr_reimbursement_line
