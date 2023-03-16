74cc9699532ff375ef00d67a2b748d0cb445c13f
mikevhe18 <viriyananda.michael@gmail.com>
2023-03-16 16:41:45 +0700

    [ssi_hr_expense_account_cash_advance] 14.0.1.0.0

--- ssi_hr_expense_account_cash_advance/models/employee_expense_account.py
+++ ssi_hr_expense_account_cash_advance/models/employee_expense_account.py
@@ -2,12 +2,11 @@
 # Copyright 2023 PT. Simetri Sinergi Indonesia
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
-class EmployeeExpenseAccountInherit(models.Model):
-    _name = "employee_expense_account"
-    _inherit = ["employee_expense_account"]
+class EmployeeExpenseAccount(models.Model):
+    _inherit = "employee_expense_account"
 
     cash_advance_settlement_line_ids = fields.One2many(
         comodel_name="hr.cash_advance_settlement_line",
@@ -15,12 +14,30 @@ class EmployeeExpenseAccountInherit(models.Model):
         domain="[('cash_advance_settlement_id.state', '!=', 'cancel')]",
         string="Cash Advance Settlement Lines",
     )
+
+    @api.depends(
+        "cash_advance_settlement_line_ids",
+        "cash_advance_settlement_line_ids.price_subtotal",
+        "cash_advance_settlement_line_ids.cash_advance_settlement_id.state",
+    )
+    def _compute_cash_advance(self):
+        for record in self:
+            result = 0.0
+            for line in record.cash_advance_settlement_line_ids.filtered(
+                lambda x: x.reimbursement_id.state not in ("terminate", "cancel")
+            ):
+                result += line.price_subtotal
+            record.amount_cash_advance = result
+            record._compute_amount()
+
     amount_cash_advance = fields.Monetary(
         compute="_compute_cash_advance",
         store=True,
-        string="Amount Cash Advance",
+        string="Cash Advance",
     )
 
-    def _compute_cash_advance(self):
-        for record in self:
-            record.amount_cash_advance = 0.0
+    def _get_expense_fields(self):
+        _super = super(EmployeeExpenseAccount, self)
+        res = _super._get_expense_fields()
+        res.append("amount_cash_advance")
+        return res
--- ssi_hr_expense_account_cash_advance/models/hr_cash_advance_settlement_line.py
+++ ssi_hr_expense_account_cash_advance/models/hr_cash_advance_settlement_line.py
@@ -2,60 +2,81 @@
 # Copyright 2023 PT. Simetri Sinergi Indonesia
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import api, fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
 
 
 class HrCashAdvanceSettlementLineInherit(models.Model):
-    _name = "hr.cash_advance_settlement_line"
     _inherit = "hr.cash_advance_settlement_line"
 
     require_expense_account = fields.Boolean(
-        string="Require Expense Account", readonly=False
+        string="Require Expense Account",
+        readonly=False,
     )
     expense_account_id = fields.Many2one(
-        comodel_name="employee_expense_account", string="Expense Account", readonly=True
+        comodel_name="employee_expense_account",
+        string="Expense Account",
+        readonly=True,
     )
 
-    @api.onchange("product_id")
+    def _check_expense_account(self):
+        self.ensure_one()
+        result = True
+        if self.require_expense_account:
+            if not self.expense_account_id:
+                error_msg = _("No expense account")
+                raise UserError(error_msg)
+
+            if self.expense_account_id.amount_residual < 0.0:
+                error_msg = _("Insuficient expense account")
+                raise UserError(error_msg)
+        return result
+
+    @api.onchange(
+        "product_id",
+    )
     def onchange_require_expense_account(self):
         result = False
 
-        if self.type_id and self.product_id:
-            expense_products = self.search(
-                [
-                    ("type_id", "=", self.type_id.id),
-                    ("product_id", "=", self.product_id.id),
-                ]
-            )
-            if len(expense_products) > 0:
-                result = True
-            expense_categories = self.search(
-                [
-                    ("type_id", "=", self.type_id.id),
-                    ("categ_id", "=", self.product_id.categ_id.id),
-                ]
-            )
-            if len(expense_categories) > 0:
-                result = True
+        if self.type_id:
+            if self.product_id:
+                expense_products = self.search(
+                    [
+                        ("type_id", "=", self.type_id.id),
+                        ("product_id", "=", self.product_id.id),
+                    ]
+                )
+                if len(expense_products) > 0:
+                    result = True
+                else:
+                    ExpTypeProdCateg = self.env["hr.expense_type_product_category"]
+                    expense_categories = ExpTypeProdCateg.search(
+                        [
+                            ("type_id", "=", self.type_id.id),
+                            ("categ_id", "=", self.product_id.categ_id.id),
+                        ]
+                    )
+                    if len(expense_categories) > 0:
+                        result = True
 
         self.require_expense_account = result
 
     @api.onchange("product_id", "account_id", "require_expense_account")
     def onchange_expense_account(self):
         self.expense_account_id = False
+        domain = []
+        if self.account_id:
+            domain = [
+                ("employee_id", "=", self.cash_advance_settlement_id.employee_id.id),
+                ("state", "=", "open"),
+                ("type_id.account_ids", "in", [self.account_id.id]),
+                ("date_start", "<=", self.cash_advance_settlement_id.date),
+                "|",
+                ("date_end", "=", False),
+                ("date_end", ">=", self.cash_advance_settlement_id.date),
+            ]
 
-        domain = [
-            ("employee_id", "=", self.reimbursement_id.employee_id.id),
-            ("state", "=", "open"),
-            ("type_id.account_ids.ids", "in", self.account_id.id),
-            (
-                "|"("date_start", "<=", self.reimbursement_id.date_expense),
-                ("date_end", ">=", self.reimbursement_id.date_expense),
-            ),
-            ("date_end", "=", False),
-        ]
-
-        if self.require_expense_account:
-            expense_accounts = self.env["employee_expense_account"].search(domain)
-            if len(expense_accounts) > 0:
-                self.expense_account_id = expense_accounts[0]
+            if self.require_expense_account:
+                expense_accounts = self.env["employee_expense_account"].search(domain)
+                if len(expense_accounts) > 0:
+                    self.expense_account_id = expense_accounts[0]
