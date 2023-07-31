10eb760d76a7c4d0111a36592dc70e7ded6ca83a
mikevhe18 <viriyananda.michael@gmail.com>
2023-07-31 16:09:48 +0700

    [ssi_hr_expense_account_reimbursement] Publish to Pypi

--- ssi_hr_expense_account_reimbursement/__manifest__.py
+++ ssi_hr_expense_account_reimbursement/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Employee Expense Account - Reimbursement Integration",
-    "version": "14.0.1.1.0",
+    "version": "14.0.1.1.1",
     "website": "https://simetri-sinergi.id",
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
     "license": "LGPL-3",
