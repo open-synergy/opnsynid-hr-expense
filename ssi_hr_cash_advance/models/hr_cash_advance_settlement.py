8a3c50bfaaacdee3dfde6c96eca44b8b2dcd566c
Andhitia Rama <andhitia.r@gmail.com>
2024-02-04 17:31:09 +0700

    [UPD] ssi_hr_cash_advance
    * Remove state field on cash advance and cash advance settlement. This will avoid log warning
    * Implement _reconfigure_statusbar_visible to arrage state statusbar on form

--- ssi_hr_cash_advance/models/hr_cash_advance_settlement.py
+++ ssi_hr_cash_advance/models/hr_cash_advance_settlement.py
@@ -1,17 +1,20 @@
 # Copyright 2022 OpenSynergy Indonesia
 # Copyright 2022 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
 from odoo import _, api, fields, models
 from odoo.exceptions import UserError
 from odoo.tools.safe_eval import safe_eval
+
+from odoo.addons.ssi_decorator import ssi_decorator
 
 
 class HrCashAdvanceSettlement(models.Model):
     _name = "hr.cash_advance_settlement"
     _inherit = [
-        "mixin.transaction_confirm",
-        "mixin.transaction_done",
         "mixin.transaction_cancel",
+        "mixin.transaction_done",
+        "mixin.transaction_confirm",
         "mixin.employee_document",
         "mixin.company_currency",
     ]
@@ -237,19 +240,6 @@ class HrCashAdvanceSettlement(models.Model):
         compute="_compute_allowed_analytic_account_ids",
         store=False,
     )
-    state = fields.Selection(
-        string="State",
-        default="draft",
-        required=True,
-        readonly=True,
-        selection=[
-            ("draft", "Draft"),
-            ("confirm", "Waiting for Approval"),
-            ("done", "Done"),
-            ("cancel", "Cancelled"),
-            ("reject", "Rejected"),
-        ],
-    )
 
     @api.model
     def _get_policy_field(self):
@@ -408,3 +398,9 @@ class HrCashAdvanceSettlement(models.Model):
     def onchange_line_analytic_account_id(self):
         if self.type_id:
             self.line_ids.analytic_account_id = False
+
+    @ssi_decorator.insert_on_form_view()
+    def _insert_form_element(self, view_arch):
+        if self._automatically_insert_view_element:
+            view_arch = self._reconfigure_statusbar_visible(view_arch)
+        return view_arch
