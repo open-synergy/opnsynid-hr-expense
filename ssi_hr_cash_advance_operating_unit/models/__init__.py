0a824fe5d62feee92648a109540c80642f679e82
Andhitia Rama <andhitia.r@gmail.com>
2026-05-12 22:55:10 +0700

    [UPD] ssi_hr_cash_advance_operating_unit

--- ssi_hr_cash_advance_operating_unit/models/__init__.py
+++ ssi_hr_cash_advance_operating_unit/models/__init__.py
@@ -2,7 +2,7 @@
 # Copyright 2026 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import (
+from . import (  # noqa: F401
     hr_cash_advance,
     hr_cash_advance_settlement,
 )
