8f6dd85d8f70dc4c3f2f6924d5f9f033962856c5
Andhitia Rama <andhitia.r@gmail.com>
2026-08-10 13:11:40 +0700

    [UPD] ssi_hr_expense_account_cash_advance
    * Lengkapi cakupan unit test compute dan onchange yang sebelumnya
      belum diuji (issue #202):
      - _compute_cash_advance dan
        _compute_valid_cash_advance_settlement_line_ids pada
        employee_expense_account
      - onchange_expense_account pada hr.cash_advance_settlement
      - onchange_require_expense_account pada
        hr.cash_advance_settlement_line
    * Seluruh skenario ditulis YAML (odoo-yaml-test) dengan
      options.auto_refresh, tanpa invalidate_cache manual.

---
+++ ssi_hr_expense_account_cash_advance/tests/test_hr_cash_advance_settlement.py
@@ -0,0 +1,18 @@
+# Copyright 2026 OpenSynergy Indonesia
+# Copyright 2026 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo_yaml_test import YamlTransactionCase
+
+from odoo.tests import tagged
+
+
+@tagged("post_install", "-at_install")
+class TestHrCashAdvanceSettlement(YamlTransactionCase):
+    """Cover the ``hr.cash_advance_settlement`` onchange extension added by
+    this module (issue open-synergy/opnsynid-hr-expense#202).
+    """
+
+    def test_hr_cash_advance_settlement_onchange_expense_account(self):
+        """Run the ``onchange_expense_account`` YAML scenario."""
+        self.run_yaml_scenario("test_data_hr_cash_advance_settlement_onchange.yaml")
