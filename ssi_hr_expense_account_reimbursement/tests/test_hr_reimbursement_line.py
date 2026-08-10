78a089980b15633af4850b42950bcb884d3ef23e
Andhitia Rama <andhitia.r@gmail.com>
2026-08-10 13:57:06 +0700

    [UPD] ssi_hr_expense_account_reimbursement
    * Menambahkan skenario unit test YAML untuk cakupan compute dan onchange:
      - _compute_reimbursement dan _compute_valid_reimbursement_line_ids
        pada employee_expense_account
      - onchange_expense_account pada hr.reimbursement
      - onchange_require_expense_account pada hr.reimbursement_line
    * Menutup 4 peringatan cakupan validator unit-test dari sapuan
      audit-sweep.sh 14.0 (open-synergy/opnsynid-hr-expense#203)

---
+++ ssi_hr_expense_account_reimbursement/tests/test_hr_reimbursement_line.py
@@ -0,0 +1,20 @@
+# Copyright 2026 OpenSynergy Indonesia
+# Copyright 2026 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo_yaml_test import YamlTransactionCase
+
+from odoo.tests import tagged
+
+
+@tagged("post_install", "-at_install")
+class TestHrReimbursementLine(YamlTransactionCase):
+    """Cover the ``hr.reimbursement_line`` onchange extension added by
+    this module (issue open-synergy/opnsynid-hr-expense#203).
+    """
+
+    def test_hr_reimbursement_line_onchange_require_expense_account(self):
+        """Run the ``onchange_require_expense_account`` YAML scenario."""
+        self.run_yaml_scenario(
+            "test_data_hr_reimbursement_line_onchange_require_expense_account.yaml"
+        )
